f8a6b81a4e613c7e42c3a954984b7dec877a6cbb
gitlwq <lwq1314520.>
2017-07-04 09:48:11 +0800

    add zhu shi

---
+++ src/main/java/com/icmt/authorization/filter/CrossFilter.java
@@ -0,0 +1,28 @@
+package com.icmt.authorization.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CrossFilter extends OncePerRequestFilter {
+
+	/** lwq
+	 * 跨域设置
+	 */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+       
+            // CORS "pre-flight" request
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            response.addHeader("Access-Control-Allow-Headers", "x-requested-with,Content-Type,authorization,Accept, Origin, XRequestedWith, LastModified");
+            response.addHeader("Access-Control-Max-Age", "3600");//60 min
+            
+            filterChain.doFilter(request, response);
+    }
+}
